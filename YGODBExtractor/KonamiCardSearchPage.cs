6e33fb5ab2bc5b70361ca7a0027456a69b05dc51
CamposDJoel <140183065+CamposDJoel@users.noreply.github.com>
2024-01-15 11:08:16 -0500

    Starting Search Functions for Spell and Traps

--- YGODBExtractor/KonamiCardSearchPage.cs
+++ YGODBExtractor/KonamiCardSearchPage.cs
@@ -15,6 +15,10 @@ namespace YGODBExtractor
 
         public static string Xpath_SearchButton = "//div[@id=\"submit_area\"]//span[.='Search']";
         public static string Xpath_MonsterGroup = "//li/span[.='Monster Cards']";
+        public static string Xpath_SpellGroup = "//li/span[.='Spell Cards']";
+        public static string Xpath_TrapGroup = "//li/span[.='Trap Cards']";
+
+
         public static string Xpath_WarriorTypeButton = "//li[@class=\"species_4_en\"]//span";
         public static string Xpath_BeastTypeButton = "//li[@class=\"species_6_en\"]//span";
 
@@ -49,6 +53,31 @@ namespace YGODBExtractor
             if (group == CardGroup.Beast_Monsters) { Element.ClickByXpath(Xpath_BeastTypeButton); }
             else if (group == CardGroup.Warrior_Monsters) { Element.ClickByXpath(Xpath_WarriorTypeButton); }
             else 
+            {
+                Element.ClickByXpath("//span[contains(text(), \"" + groupString + "\")]");
+            }
+
+            //Click Search
+            Element.ClickByXpath(Xpath_SearchButton);
+
+            //Call the CardListPage to wait until that page is loaded
+            KonamiCardListPage.WaitUntilPageIsLoaded();
+
+            //Change the view to 100 cards per page as a list
+            KonamiCardListPage.Set100ItemsPerPageView();
+        }
+        public static void SearchSpellCard(CardGroup group)
+        {
+            //Convert the group enum to its string counterpart
+            string groupString = GlobalData.CardGroupToString(group);
+
+            //Click the Monster Group Tab
+            Element.ClickByXpath(Xpath_MonsterGroup);
+
+            //Click the respective group
+            if (group == CardGroup.Beast_Monsters) { Element.ClickByXpath(Xpath_BeastTypeButton); }
+            else if (group == CardGroup.Warrior_Monsters) { Element.ClickByXpath(Xpath_WarriorTypeButton); }
+            else
             {
                 Element.ClickByXpath("//span[contains(text(), \"" + groupString + "\")]");
             }
